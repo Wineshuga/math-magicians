baf5f746c4660f2a19297c525ade0a128cd104c9
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-09 21:31:40 +0100

    Fix 'x' to the right operator symbol

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Keys = () => {
+const Keys = ({ click }) => {
   const keys = [
-    'AC', '+/-', '%', '÷',
-    '7', '8', '9', '×',
-    '4', '5', '6', '-',
-    '1', '2', '3', '+',
-    '0', '.', '=',
+    'AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',
   ];
 
   return (
     <div className="keys">
       {keys.map((key) => (
-        <button key={key} type="button" className={`key ${key === '0' ? 'zero' : ''} ${['+', '-', '÷', '×', '='].includes(key) ? 'orange-key' : 'grey-key'}`}>
+        <button
+          key={key}
+          type="button"
+          className={`key ${key === '0' ? 'zero' : ''} ${['+', '-', '÷', 'x', '='].includes(key) ? 'orange-key' : 'grey-key'}`}
+          onClick={() => click(key)}
+        >
           {key}
         </button>
       ))}
     </div>
   );
+};
+
+Keys.propTypes = {
+  click: PropTypes.func.isRequired,
 };
 
 export default Keys;

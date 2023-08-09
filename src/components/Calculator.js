ea83c796ac51c0ad0bc6d8f009fb5258be449986
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-09 21:30:13 +0100

    Add calculator functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Keys from './Keys';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calc">
-    <div className="output">0</div>
-    <Keys />
-  </div>
-);
+const Calculator = () => {
+  const [showInput, setShowInput] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (key) => {
+    const calc = calculate(showInput, key);
+    setShowInput(calc);
+  };
+
+  return (
+    <div className="calc">
+      <div className="output">
+        {showInput.total}
+        {' '}
+        {showInput.operation}
+        {' '}
+        {showInput.next}
+      </div>
+      <Keys click={handleClick} />
+    </div>
+  );
+};
 
 export default Calculator;

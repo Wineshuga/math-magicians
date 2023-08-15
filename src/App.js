7991c1ec5e91828bc8e361a78eff0d6674df2541
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-15 15:56:50 +0100

    Create routes to the three pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import HomePage from './components/HomePage';
+
+const Layout = () => (
+  <>
+  <header>
+    <h1>Math Magicians</h1>
+    <ul>
+      <Link to="/">Home</Link>
+      <Link to="calculator">Calculator</Link>
+      <Link to="quote">Quote</Link>
+    </ul>
+  </header>
+  <Outlet />
+  </>
+)
 
 function App() {
   return (
-    <main>
-      <Quote />
-      <Calculator />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />}/>
+          <Route path="calculator" element={<Calculator />}/>
+          <Route path="quote" element={<Quote />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

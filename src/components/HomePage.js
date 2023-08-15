ce280077ce3369cc392deb59f038e0320a5e0c7e
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-15 15:56:20 +0100

    Make home page component

---
+++ src/components/HomePage.js
@@ -0,0 +1,21 @@
+const HomePage = () => {
+  return (
+    <div>
+        <h2>Welcome to our page!</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam sint quisquam minima at eius natus cupiditate. Quam corporis qui, cumque dicta molestias iure dolores. Vero fuga officiis mollitia eaque aperiam.
+        Est laudantium illum optio omnis quibusdam labore at sit dicta accusantium. Delectus veritatis eaque inventore esse cumque. Maiores corrupti labore veniam autem sequi tenetur quia quae. Nam voluptatem illum ipsam!
+        Inventore atque dolores, commodi molestiae provident quos recusandae facere sequi illum. Laboriosam commodi itaque, a minima distinctio officiis. Explicabo, itaque maiores rem dolor laboriosam voluptatum non fugiat sint quisquam! Ea!
+        Vel veritatis, illo consequuntur eaque maiores corporis accusantium optio nobis! Sed voluptatem hic excepturi corporis harum natus obcaecati commodi quod culpa aliquid, amet vero et perspiciatis blanditiis? Alias, veniam adipisci!
+        Quam fugit dolores inventore sed, eos in vel optio ratione unde, velit voluptatem! Necessitatibus quisquam repellendus beatae dolore quasi quia. Alias magnam, quis impedit doloremque perferendis deserunt ducimus porro ex.
+        </p>
+        <p>
+        Omnis doloribus accusamus alias dolore distinctio? Iusto aliquid, unde nam assumenda saepe, accusamus quo quos inventore ipsa aspernatur beatae earum delectus, dolores dolorem eaque libero asperiores labore recusandae. Non, placeat.
+        Impedit repudiandae deserunt doloribus magnam fuga quo saepe labore, voluptatem ratione explicabo voluptate ipsum eius aliquid obcaecati doloremque! Ducimus ex repellendus debitis iusto provident impedit cum, obcaecati officiis eius natus.
+        Pariatur odio id cumque rerum modi similique commodi odit vitae ipsa rem nobis provident architecto ex vero, corporis accusantium harum veniam excepturi libero fuga minus consequatur beatae? Nobis, molestias voluptate!
+        Molestias itaque soluta beatae eveniet laboriosam? In cumque magnam maxime quisquam consequatur laboriosam dolorum eveniet, culpa accusamus quibusdam dolore quia ullam alias cupiditate nam fugit error modi perferendis aperiam? Repellendus?
+        Totam laborum quis eum obcaecati officiis veniam nulla nihil id nam iusto molestias mollitia modi aperiam eligendi consequatur explicabo, nobis fugit delectus nemo reiciendis dicta hic? Quidem laudantium voluptas doloremque?</p>
+    </div>
+  )
+}
+
+export default HomePage
